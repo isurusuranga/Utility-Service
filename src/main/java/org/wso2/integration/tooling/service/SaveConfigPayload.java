3d4be87e8ea6ed2082b94116b12c84ba9f57c909
Nadeeshaan <nadeeshaangunasinghe@gmail.com>
2016-10-03 18:21:10 +0530

    Initial Commit

---
+++ src/main/java/org/wso2/integration/tooling/service/SaveConfigPayload.java
@@ -0,0 +1,17 @@
+package org.wso2.integration.tooling.service;
+
+/**
+ * Created by nadeeshaan on 10/2/16.
+ */
+public class SaveConfigPayload {
+    private String location;
+    private String config;
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getConfig() {
+        return config;
+    }
+}
